1d693da1b4c8c25197ffc7d21cae1b27bd29b6a8
ironhead <1784493817@qq.com>
2018-11-04 16:06:41 +0800

    基本啥时没干，一直琢磨怎么把SplitPane的窗口大小和父节点的大小数据绑定，Ccontroller试了半天属性是只读的，后来发现它是自适应的

--- src/TodoList/ironhead/top/Main.java
+++ src/TodoList/ironhead/top/Main.java
@@ -1,9 +1,11 @@
 package TodoList.ironhead.top;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
